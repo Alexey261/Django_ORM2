78d16e591e953fc69c85b3e01d2db1a91899ab6a
Алексей <dlyx@yandex.ru>
2024-11-24 10:34:08 +0200

    .

--- task1/models.py
+++ task1/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
