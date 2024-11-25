3b5a2303591dec16e269d57128a9f7ebe199247d
Алексей <dlyx@yandex.ru>
2024-11-25 06:13:32 +0200

    ..

--- task1/models.py
+++ task1/models.py
@@ -4,6 +4,7 @@ class Buyer(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     age = models.PositiveIntegerField()
+    password = models.CharField(max_length=30, default="")
 
     def __str__(self):
         return self.name
--- task1/views.py
+++ task1/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 # from .forms import UserRegister
+from task1.models import *
 
-users = [['Vasya', 'xyz717~!@', 18]]
+
+def index(request):
+    return render(request, 'platform.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def games(request):
+    # gds = ['Counter-Strike', 'Quake III Arena', 'Doom 2: Hell on Earth']
+    gds = Game.objects.all()
+    data = {'goods': gds}
+    return render(request, 'games.html', context=data)
+
+
+
+# users = [['Vasya', 'xyz717~!@', 18]]
 
 def sign_up(request):
 
@@ -11,8 +27,8 @@ def sign_up(request):
         password = request.POST.get('password')
         repeat_password = request.POST.get('repeat_password')
         age = request.POST.get('age')
-
-        if any(l[0] == login for l in users):
+        users = Buyer.objects.all()
+        if any(l.name == login for l in users):
             err_msg = 'Пользователь уже существует'
         elif not(password == repeat_password):
             err_msg = 'Пароли не совпадают'
@@ -20,12 +36,13 @@ def sign_up(request):
             err_msg = 'Вы должны быть старше 18'
         else:
             err_msg = 'Привет, '
-            users.append([login, password, int(age)])
+            # users.append([login, password, int(age)])
+            Buyer.objects.create(name=login, balance=0, age=age, password=password)
 
         info['error'] = err_msg
         info['user'] = [login, password, repeat_password, age]
 
-        return render(request, 'fifth_task/registration_page.html', context=info)
+        return render(request, 'registration_page.html', context=info)
 
-    return render(request, 'fifth_task/registration_page.html')
+    return render(request, 'registration_page.html')
 
